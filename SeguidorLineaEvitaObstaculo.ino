408ad973d0ec1380901bd56921400907d0c3f836
Grillo <carlsenrmt26@gmail.com>
2022-07-02 16:31:08 -0400

    code finish

--- SeguidorLineaEvitaObstaculo.ino
+++ SeguidorLineaEvitaObstaculo.ino
@@ -1,91 +1,157 @@
-#include<NewPing.h>
 
-// Motor 1
-int ENA = 10;
-int IN1 = 9;
-int IN2 = 8;
+int aM1 = 6;
+int rM1 = 7;
+int aM2 = 4;
+int rM2 = 5;
 
-// Motor 2
-int ENB = 5;
-int IN3 = 7;
-int IN4 = 6;
+int ledIzq = 9;
+int ledDer = 8;
+int ledVerde = 10;
+int ledRojo = 11;
 
-// Se definen conexiones del Sensor HC-SR04 con Arduino
+int trig = 2;
+int echo = 3;
+int tiempo;
+int distancia;
 
-#define TRIG A2 // PIN TRIGGER connectado al pin analogico A2
-#define ECHO A3 // PIN ECHO conectado al pin analogico A3
-#define MAX_DISTANCE 100 // Define la Maxima Distancia
 
-NewPing sonar(TRIG, ECHO, MAX_DISTANCE); 
 
-void setup() {
- pinMode (ENA, OUTPUT);
- pinMode (ENB, OUTPUT);
- pinMode (IN1, OUTPUT);
- pinMode (IN2, OUTPUT);
- pinMode (IN3, OUTPUT);
- pinMode (IN4, OUTPUT);  
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(aM1, OUTPUT);
+  pinMode(rM1, OUTPUT);
+  pinMode(aM2, OUTPUT);
+  pinMode(rM2, OUTPUT);
+  
+  pinMode(ledIzq, OUTPUT);
+  pinMode(ledDer, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledRojo, OUTPUT);
+  
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  
 }
 
-void loop() {
-  delay(70);
-  //Izquierda();//-->Adelante
-  Derecha(); //-->Atras
-  //Atras();
-  //Adelante();
+void loop()
+{
+  //i = Contador de los lados
+  for(int i=1; i<=4; i++){
+    Serial.print("Lado N ");
+    Serial.println(i);
+    
+    girarCuadrado(); 
+    
+  }
+  Stop();
+  Serial.println("Stop...");
+  delay(5000);
+    
+}
+//<------ Gira en Cuadrado 1 metro---->
+void girarCuadrado(){
+    Adelante();
+    buscarObjeto();//<------
+    delay(5000);
+    Izquierda();
+    delay(3000);
+}
+//<-----Busca Obstaculo en sensor---->
+void buscarObjeto(){
+  digitalWrite(trig,HIGH);
+  delay(1);
+  digitalWrite(trig,LOW);
+  tiempo = pulseIn(echo,HIGH);
+  distancia = tiempo/58.2; //Tiempo en segundos
+  Serial.println(distancia);
+  delay(500);
+  
+  if(distancia <50){
+        Serial.println("Objeto Detectado");
+      esquivar();
+    }
+}
+//<-----Esquiva Obstaculo---->
+void esquivar(){
+    Serial.println("Esquivando...");
+    Stop();
+    delay(3000);
+    Derecha();
+    delay(3000);
+    Adelante();
+    delay(5000);
+    Izquierda();
+    delay(3000);
+    Adelante();
+    delay(5000);
+    Izquierda();
+    delay(3000);
+    Adelante();
+    delay(5000);
+    Derecha();
+    delay(3000);
 }
 
-void Adelante (){
- //Dirección motor A
- digitalWrite (IN1, LOW);
- digitalWrite (IN2, HIGH);
- analogWrite (ENA, 255); //Velocidad motor A
- //Dirección motor B
- digitalWrite (IN3, HIGH);
- digitalWrite (IN4, LOW);
- analogWrite (ENB, 255); //Velocidad motor B
+
+//<-------- Motores -------->
+void Adelante(){
+digitalWrite(ledVerde, HIGH);
+digitalWrite(ledRojo, LOW);
+digitalWrite(ledDer, LOW);
+digitalWrite(ledIzq, LOW);
+  
+digitalWrite(rM1, LOW);
+digitalWrite(rM2, LOW);
+digitalWrite(aM1, HIGH);
+digitalWrite(aM2, HIGH);
 }
 
-void Derecha (){
- //Dirección motor A
- digitalWrite (IN1, HIGH);
- digitalWrite (IN2, LOW);
- analogWrite (ENA, 255); //Velocidad motor A
- //Dirección motor B
- digitalWrite (IN3, HIGH);
- digitalWrite (IN4, LOW);
- analogWrite (ENB, 255); //Velocidad motor A
+void Atras(){
+digitalWrite(ledVerde, LOW);
+digitalWrite(ledRojo, LOW);
+digitalWrite(ledDer, HIGH);
+digitalWrite(ledIzq, HIGH);
+
+digitalWrite(aM1, LOW);
+digitalWrite(aM2, LOW);
+digitalWrite(rM1, HIGH);
+digitalWrite(rM2, HIGH);
 }
 
-void Izquierda (){
- //Dirección motor A
- digitalWrite (IN1, LOW);
- digitalWrite (IN2, HIGH);
- analogWrite (ENA, 255); //Velocidad motor A
- //Dirección motor B
- digitalWrite (IN3, LOW);
- digitalWrite (IN4, HIGH);
- analogWrite (ENB, 255); //Velocidad motor A
+void Izquierda(){
+digitalWrite(ledVerde, LOW);
+digitalWrite(ledRojo, LOW);
+digitalWrite(ledDer, LOW);
+digitalWrite(ledIzq, HIGH);
+  
+digitalWrite(aM1, LOW);
+digitalWrite(aM2, HIGH);
+  
+
+
 }
 
-void Atras (){
- //Dirección motor A
- digitalWrite (IN1, HIGH);
- digitalWrite (IN2, LOW);
- analogWrite (ENA, 255); //Velocidad motor A
- //Dirección motor B
- digitalWrite (IN3, LOW);
- digitalWrite (IN4, HIGH);
- analogWrite (ENB, 255); //Velocidad motor B
+void Derecha(){
+digitalWrite(ledVerde, LOW);
+digitalWrite(ledRojo, LOW);
+digitalWrite(ledIzq, LOW);
+digitalWrite(ledDer, HIGH);
+  
+digitalWrite(aM2, LOW);
+digitalWrite(aM1, HIGH);
 }
 
-void Parar (){
- //Dirección motor A
- digitalWrite (IN1, LOW);
- digitalWrite (IN2, LOW);
- analogWrite (ENA, 0); //Velocidad motor A
- //Dirección motor B
- digitalWrite (IN3, LOW);
- digitalWrite (IN4, LOW);
- analogWrite (ENB, 0); //Velocidad motor A
-} 
+void Stop(){
+digitalWrite(ledVerde, LOW);
+digitalWrite(ledRojo, HIGH);
+digitalWrite(ledIzq, HIGH);
+digitalWrite(ledDer, HIGH);
+  
+digitalWrite(aM1, LOW);
+digitalWrite(aM2, LOW);
+digitalWrite(rM1, LOW);
+digitalWrite(rM2, LOW);
+
+
+}
